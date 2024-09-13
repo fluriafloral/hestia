da76a82e89f879bfa708030787a1a66f20f68c3b
fluriafloral <lampia131@gmail.com>
2024-09-12 23:45:09 -0300

    feat: Tests for Reserve Repo

--- src/test/java/reserves/internal/reserve/ReserveRepositoryTest.java
+++ src/test/java/reserves/internal/reserve/ReserveRepositoryTest.java
@@ -1,16 +1,35 @@
 package reserves.internal.reserve;
 
-import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class ReserveRepositoryTest {
+
+    @Autowired
+    private ReserveRepository reserveRepo;
     
+    private LocalDate start;
+    private LocalDate end;
+
+    @BeforeEach 
+    public void setUp(){
+        start = LocalDate.of(2000, 01, 01);
+        end = LocalDate.of(2000, 01, 02);
+    }
+
     @Test
     void testFindByEnd() {
-
+        assertTrue(reserveRepo.findByEnd(end).stream().allMatch(reserve -> reserve.getEnd().equals(end)));
     }
 
     @Test
     void testFindByStart() {
-
+        assertTrue(reserveRepo.findByStart(start).stream().allMatch(reserve -> reserve.getStart().equals(start)));
     }
 }
