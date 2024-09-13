051f57ed893d90b0c33b0dc23be7db0d6dd0829d
fluriafloral <lampia131@gmail.com>
2024-09-12 23:45:37 -0300

    feat: Tests for Reserve Entity

--- src/test/java/reserves/internal/reserve/ReserveTest.java
+++ src/test/java/reserves/internal/reserve/ReserveTest.java
@@ -1,16 +1,34 @@
 package reserves.internal.reserve;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import reserves.internal.guest.Guest;
 
 public class ReserveTest {
     
-    @Test
-    void testAddGuest() {
+    private Reserve reserve;
 
+    private Guest newGuest;
+
+    @BeforeEach
+    public void setUp() {
+        reserve = new Reserve(new ArrayList<>());
+        newGuest = new Guest("John Doe", "123456789");
     }
 
     @Test
-    void testRemoveGuest() {
+    void testAddnewGuest() {
+        reserve.addGuest(newGuest);
+        assertTrue(reserve.getGuests().stream().anyMatch(guest -> guest.getDocumentNumber().equals(newGuest.getDocumentNumber())));
+    }
 
+    @Test
+    void testRemovenewGuest() {
+        reserve.removeGuest(newGuest.getDocumentNumber());
+        assertTrue(reserve.getGuests().stream().noneMatch(guest -> guest.getDocumentNumber().equals(newGuest.getDocumentNumber())));
     }
 }
