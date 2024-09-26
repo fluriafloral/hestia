2e35ed6cdc6af5e2a8a3e9fbf3fc792f2fc7fa19
fluriafloral <lampia131@gmail.com>
2024-09-26 17:37:22 -0300

    fix: dailyRate imports

--- src/test/java/reserves/internal/dailyRate/DailyRateRepositoryTest.java
+++ src/test/java/reserves/internal/dailyRate/DailyRateRepositoryTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import reserves.internal.dailyRate.DailyRate;
-import reserves.internal.dailyRate.DailyRateRepository;
-
 @DataJpaTest
 public class DailyRateRepositoryTest {
 
